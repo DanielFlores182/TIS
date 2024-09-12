06836aa7a1a74b785a6cb9f3a237b16e826e97e1
Giselle-Lorena <161660840+Giselle-Lorena@users.noreply.github.com>
2024-09-11 21:07:20 -0400

    Primer layout
    Faltan mejoras en el menu lateral

---
+++ soda-corp/resources/views/docente/registrarplanilla.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.plantilla')
+
+@section('links')
+    <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+@endsection
+
+@section('titulo', 'Registrar')
+
+@section('contenido')
+    
+@endsection
