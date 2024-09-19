d756d33be26f72af88589cc34c243249f9c0f287
Andrea <andrea.calle.rojas666@gmail.com>
2024-09-18 23:04:33 -0400

    Creado modelo user

---
+++ soda-corp/app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User:: all ();
+        return users;
+    }
+    public function store(Request $request){
+        $user = new User($request);
+        $user -> save();
+        return response()->json([
+            'status' => 200, 
+            'message' => "Registro exitoso"
+        ]);
+    }
+    public function show($id){
+        $user = User:: find($id);
+        return user;
+    }
+}
